46bb35577972fd0bdf1bb9d1107c04c185f68856
Nathan Waddington <nathan_waddington@alumni.sfu.ca>
2018-01-31 07:50:20 -0800

    starting to add button functionality

--- demos/rgb/sketch/sketch.ino
+++ demos/rgb/sketch/sketch.ino
@@ -7,8 +7,19 @@ WebUSB WebUSBSerial(1, "webusb.github.io/arduino/demos");
 const int redPin = 9;
 const int greenPin = 10;
 const int bluePin = 11;
+const int buttonPin = 2;    // the number of the pushbutton pin
+const int ledPin = 13;      // the number of the indicator LED pin
+
+
 int color[3];
 int index;
+
+int ledState = HIGH;        // the current state of the output pin
+int buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
 
 void setup() {
   while (!Serial) {
@@ -18,9 +29,54 @@ void setup() {
   Serial.write("Sketch begins.\r\n");
   Serial.flush();
   index = 0;
+
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
 }
 
 void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only send a message if the button state has changed
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+
+        if (Serial && Serial.available()) {
+          Serial.print("Indicator LED state: ");
+          Serial.print(ledState);
+          Serial.print(".\r\n");
+          Serial.flush();
+        }
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+
+
+  // webUSB serial stuff
   if (Serial && Serial.available()) {
     color[index++] = Serial.read();
     if (index == 3) {
